0f816da8ef168113572adee2136146231dda1527
Marwan Alani <marwan@alani.ca>
2017-07-26 23:05:08 -0400

    HackerRank practice

--- src/ca/alani/Main.java
+++ src/ca/alani/Main.java
@@ -5,9 +5,9 @@ import java.util.*;
 public class Main {
     public static void main(String []args) {
         Scanner sc = new Scanner(System.in);
-        int lineCount = 1;
+        int lineCount = 0;
         while (sc.hasNext()) {
-            System.out.println(Integer.toString(lineCount++) + " " + sc.nextLine());
+            System.out.println(Integer.toString(++lineCount) + " " + sc.nextLine());
         }
     }
 }
