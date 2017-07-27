4d4fe25963cb8ab21edc8a100143cbb1d993fc06
Marwan Alani <marwan@alani.ca>
2017-07-27 16:52:09 -0400

    HackerRank Java Practice

--- src/ca/alani/Main.java
+++ src/ca/alani/Main.java
@@ -3,11 +3,27 @@ package ca.alani;
 import java.util.*;
 
 public class Main {
-    public static void main(String []args) {
+
+    private static int B;
+    private static int H;
+    private static boolean flag = true;
+
+    static {
         Scanner sc = new Scanner(System.in);
-        int lineCount = 0;
-        while (sc.hasNext()) {
-            System.out.println(Integer.toString(++lineCount) + " " + sc.nextLine());
+        B = sc.nextInt();
+        H = sc.nextInt();
+        if (B < 1 || H < 1) {
+            flag = false;
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
         }
     }
-}
+
+    public static void main(String[] args){
+        if(flag) {
+            int area = B * H;
+            System.out.println(area);
+        }
+    }//end of main
+
+}//end of class
+
