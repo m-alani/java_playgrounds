a03eba638acd44830759e52b56bd0a01e0215c0c
Marwan Alani <marwan@alani.ca>
2017-07-29 19:28:39 -0400

    HackerRank practice

--- src/ca/alani/Main.java
+++ src/ca/alani/Main.java
@@ -6,44 +6,14 @@ import java.security.*;
 public class Main {
 
     public static void main(String[] args) {
-
-    DoNotTerminate.forbidExit();
-
-    try {
         Scanner in = new Scanner(System.in);
-        int n = in .nextInt();
-        in.close();
-        String s = Integer.toString(n);
+        int month = Integer.parseInt(in.next()) - 1;
+        int day = Integer.parseInt(in.next());
+        int year = Integer.parseInt(in.next());
 
-
-        if (n == Integer.parseInt(s)) {
-            System.out.println("Good job");
-        } else {
-            System.out.println("Wrong answer.");
-        }
-    } catch (DoNotTerminate.ExitTrappedException e) {
-        System.out.println("Unsuccessful Termination!!");
-        }
-    }
-}
-
-//The following class will prevent you from terminating the code using exit(0)!
-class DoNotTerminate {
-
-    public static class ExitTrappedException extends SecurityException {
-
-        private static final long serialVersionUID = 1;
-    }
-
-    public static void forbidExit() {
-        final SecurityManager securityManager = new SecurityManager() {
-            @Override
-            public void checkPermission(Permission permission) {
-                if (permission.getName().contains("exitVM")) {
-                    throw new ExitTrappedException();
-                }
-            }
-        };
-        System.setSecurityManager(securityManager);
+        Calendar calendar = new GregorianCalendar();
+        calendar.set(year, month, day);
+        String weekdays[] = {"SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"};
+        System.out.println(weekdays[calendar.get(Calendar.DAY_OF_WEEK) - 1]);
     }
 }
