12e2479f1b41ec013d2cba815f3bf15421ebc52c
Marwan Alani <marwan@alani.ca>
2017-07-30 14:39:50 -0400

    HackerRank Java practice

--- src/ca/alani/Main.java
+++ src/ca/alani/Main.java
@@ -1,19 +1,21 @@
 package ca.alani;
 
 import java.util.*;
-import java.security.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        // read the input
         Scanner in = new Scanner(System.in);
-        int month = Integer.parseInt(in.next()) - 1;
-        int day = Integer.parseInt(in.next());
-        int year = Integer.parseInt(in.next());
+        String first = in.nextLine();
+        String second = in.nextLine();
 
-        Calendar calendar = new GregorianCalendar();
-        calendar.set(year, month, day);
-        String weekdays[] = {"SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"};
-        System.out.println(weekdays[calendar.get(Calendar.DAY_OF_WEEK) - 1]);
+        // print the output
+        System.out.println(first.length() + second.length());
+        System.out.println((first.compareTo(second) > 0) ? "Yes" : "No");
+
+        System.out.print(first.substring(0,1).toUpperCase().concat(first.substring(1)));
+        System.out.print(" ");
+        System.out.println(second.substring(0,1).toUpperCase().concat(second.substring(1)));
     }
 }
