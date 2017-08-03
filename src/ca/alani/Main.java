dc36d6b282cbfdd77ae01a7e1add3753e9d64a2c
Marwan Alani <marwan@alani.ca>
2017-08-03 11:16:33 -0400

    HackerRank Practice

--- src/ca/alani/Main.java
+++ src/ca/alani/Main.java
@@ -4,27 +4,12 @@ import java.util.*;
 
 public class Main {
 
-    public static String getSmallestAndLargest(String s, int k) {
-        String smallest = s.substring(0,k);
-        String largest = s.substring(0,k);
-        int idx = k + 1;
-        while (idx <= s.length()) {
-            String temp = s.substring(idx-k, idx);
-            if (smallest.compareTo(temp) > 0) {
-                smallest = temp;
-            } else if (largest.compareTo(temp) < 0){
-                largest = temp;
-            }
-            idx++;
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while(testCases>0){
+            String pattern = in.nextLine();
+            //Write your code
         }
-        return smallest + "\n" + largest;
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-        int k = sc.nextInt();
-        sc.close();
-        System.out.println(getSmallestAndLargest(s, k));
     }
 }
