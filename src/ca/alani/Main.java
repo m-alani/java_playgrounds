01a412a9e6284bcb7f496f69f32a719a38b488d6
Marwan Alani <marwan@alani.ca>
2017-08-12 11:43:02 -0400

    HackerRank practice

--- src/ca/alani/Main.java
+++ src/ca/alani/Main.java
@@ -22,7 +22,7 @@ public class Main {
                 return "NO";
             }
         }
-        return "YES";
+        return stack.isEmpty() ? "YES" : "NO";
     }
 
     public static void main(String[] args) {
