c0884523401490ec2e0aed00d15e5f46d02e7e1b
Marwan Alani <marwan@alani.ca>
2017-08-20 14:30:49 -0400

    HackerRank practice

--- src/ca/alani/Main.java
+++ src/ca/alani/Main.java
@@ -30,7 +30,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
-        for(int a0 = 0; a0 < t; a0++){
+        for(int a = 0; a < t; a++){
             String s = in.next();
             String result = isBalanced(s);
             System.out.println(result);
