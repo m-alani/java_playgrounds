7602b9bd6e43263e515f9e92aa13f62babf13499
Marwan Alani <marwan@alani.ca>
2017-08-22 22:27:14 -0400

    HackerRank practice

--- src/ca/alani/Main.java
+++ src/ca/alani/Main.java
@@ -26,7 +26,7 @@ public class Main {
         return stack.isEmpty() ? "YES" : "NO";
     }
 
-    // Main function
+    // Main
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
